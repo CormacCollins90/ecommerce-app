88ce24120d67e1446a11dbf8e540c40b63fa128a
Cormac Collins <cormaccollins90@gmail.com>
2018-11-19 16:47:46 +0000

    admin setup

--- products/urls.py
+++ products/urls.py
@@ -15,15 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from accounts.views import signup
-from products.views import product_list
-from accounts.views import show_profile
+from accounts.views import signup, products_urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', product_list, name='home'),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/signup/', signup, name='signup'),
-    path('accounts/profile/', show_profile, name='profile'),
-
+    path(' ', product_list, name='product_list'),
 ]
