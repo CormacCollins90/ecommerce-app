95d06fbf5550ba01a5aa5afdc7ebfa196d728e99
Cormac Collins <cormaccollins90@gmail.com>
2018-11-22 16:30:25 +0000

    stripe installed

--- checkout/views.py
+++ checkout/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from products.models import Product
 from .forms import MakePaymentForm, OrderForm
 from checkout.models import OrderLineItem
+from django.conf import settings
+import stripe
+from django.contrib import messages
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 # Create your views here.
 
@@ -39,7 +44,7 @@ def show_checkout(request):
     
     payment_form = MakePaymentForm()
     order_form = OrderForm()
-    context = {'payment_form': payment_form, 'order_form': order_form}
+    context = {'payment_form': payment_form, 'order_form': order_form, 'publishable': settings.STRIPE_PUBLISHABLE_KEY}
     
     context.update(cart_items_and_total)
 
@@ -47,10 +52,16 @@ def show_checkout(request):
     
 def submit_payment(request):
     
+    cart = request.session.get('cart', {})
+    cart_items_and_total = get_cart_items_and_total(cart)
+    
+    payment_form = MakePaymentForm(request.POST)
     order_form = OrderForm(request.POST)
-    if order_form.is_valid():
-        order = order_form.save()
+    
+    if order_form.is_valid() and payment_form.is_valid():
         
+        # save the order to the database
+        order = order_form.save()
         cart = request.session.get('cart', {})
         for product_id, quantity in cart.items():
             line_item = OrderLineItem()
@@ -59,4 +70,32 @@ def submit_payment(request):
             line_item.order = order
             line_item.save()
         
-    return HttpResponse(str(order.id))    
+        #Grab the money and run!
+        total = cart_items_and_total['cart_total']
+        stripe_token=payment_form.cleaned_data['stripe_id']
+
+        try:
+
+            total_in_cent = int(total*100)
+            customer = stripe.Charge.create(
+                amount=total_in_cent,
+                currency="EUR",
+                description="Dummy Transaction",
+                card=stripe_token,
+            )
+
+        except stripe.error.CardError:
+            print("Declined")
+            messages.error(request, "Your card was declined!")
+
+        if customer.paid:
+            print("Paid")
+            messages.error(request, "You have successfully paid")
+        
+
+            
+            
+    # clear the cart    
+    del request.session['cart']
+    #redirect to home
+    return redirect("/")    
